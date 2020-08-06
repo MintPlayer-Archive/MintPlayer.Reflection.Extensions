e980898d7efbb4213f0d57a380546c57c1ab5139
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2020-08-06 10:55:33 +0200

    Get interfaces without basetype interfaces

---
+++ MintPlayer.Reflection.Extensions.Test/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace MintPlayer.Reflection.Extensions.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teacher = new Teacher
+            {
+                FirstName = "John",
+                LastName = "Roan",
+                ClassName = "Math"
+            };
+
+            var type = teacher.GetType();
+
+            var baseTypeInterfaces = type.BaseType.GetInterfaces();
+            var typeInterfaces = type.GetInterfaces();
+
+            var interfaces = typeInterfaces.Except(baseTypeInterfaces);
+        }
+    }
+
+
+    public interface IPerson
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+
+    public class Person : IPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public interface ITeacher : INeedsClassRoom
+    {
+        string ClassName { get; set; }
+    }
+
+    public class Teacher : Person, ITeacher
+    {
+        public string ClassName { get; set; }
+        public string ClassRoomType { get; set; }
+    }
+
+    public interface INeedsClassRoom
+    {
+        string ClassRoomType { get; set; }
+    }
+}
